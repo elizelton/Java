3ca869896772e57b267db9a1f6c0a6a3ea1694f3
elizelton <elizeltonls@gmail.com>
2018-06-20 19:06:12 -0300

    update

--- folhaDePagamentoElizelton/src/main/java/view/PrincipalController.java
+++ folhaDePagamentoElizelton/src/main/java/view/PrincipalController.java
@@ -3,11 +3,14 @@ package view;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import model.*;
 
 public class PrincipalController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        Gerente gerComercial = new Gerente("Elizelton", 10000, "Masculino", 0);
+        System.out.println(gerComercial);
+        System.out.println(gerComercial.getTipo());
     }
 }
