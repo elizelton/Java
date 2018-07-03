f0788335434132ba906d87c28265279099061058
elizelton <elizeltonls@gmail.com>
2018-07-02 22:55:48 -0300

    folha de pagamento funcinal gui

--- folhaDePagamentoElizelton/src/main/java/view/PrincipalController.java
+++ folhaDePagamentoElizelton/src/main/java/view/PrincipalController.java
@@ -12,9 +12,13 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -62,43 +66,17 @@ public class PrincipalController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
-        txtFldNomeFunc.requestFocus();
-        rdBtnGerenteSelect();
-        btnCadastrarFunc.disableProperty().bind(txtFldNomeFunc.textProperty().isEmpty().or(txtFldSalFunc.textProperty().isEmpty()));
-
-//        Gerente gerComer = new Gerente("Elizelton", 10000, "M", -1000);
-//        Gerente gerVendas = new Gerente("Julio", 12000, "M", -5000);
-//        Gerente gerAdm = new Gerente("Fernanda", 750, "F", 800);
-//        Gerente gerTi = new Gerente(" ", 8000, "F", 4000);
-//        Administrativo auxAdmComp = new Administrativo("Barbara", 3000, "F");
-//        Administrativo auxAdmRh = new Administrativo("João", 2000, "M");
-//        Administrativo auxAdmVend = new Administrativo("Jorge", 2500, "M");
-//        Vendedor vendInter = new Vendedor("Natalia", 3000, "f", -10000);
-//        Vendedor vendAgro = new Vendedor("Rafael", 3000, "MF", 50000);
-//        Vendedor vendVeic = new Vendedor("Maria", -3000, "MF", 100000);
-//        lstFuncionarios.add(gerComer);
-//        lstFuncionarios.add(gerVendas);
-//        lstFuncionarios.add(gerAdm);
-//        lstFuncionarios.add(gerTi);
-//        lstFuncionarios.add(auxAdmComp);
-//        lstFuncionarios.add(auxAdmRh);
-//        lstFuncionarios.add(auxAdmVend);
-//        lstFuncionarios.add(vendInter);
-//        lstFuncionarios.add(vendAgro);
-//        lstFuncionarios.add(vendVeic);
-        for (Funcionario func : lstFuncionarios) {
-            System.out.println(func);
-        }
-
-//        txtFldNomeFunc.textProperty().addListener(
-//                (ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
-//                    if (!newValue.matches("\\S*\\ ") && !newValue.contains(" ")) {
-//                        txtFldNomeFunc.setText(oldValue);
-//                    } else {
-//                       txtFldNomeFunc.setText(newValue.toUpperCase());
-//                    }
-//                }
-//        );
+        limparCampos();
+        txtFldNomeFunc.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                if (!newValue.matches("\\D*") || newValue.matches("\\p{Punct}")) {
+                    txtFldNomeFunc.setText(oldValue);
+                } else {
+                    txtFldNomeFunc.setText(newValue.toUpperCase());
+                }
+            }
+        });
         txtFldSalFunc.textProperty().addListener(
                 (observable, oldValue, newValue) -> {
                     if (!newValue.matches("\\d*(\\" + separadorDecimal + "\\d*)?") && !newValue.isEmpty()) {
@@ -108,6 +86,55 @@ public class PrincipalController implements Initializable {
                     }
                 }
         );
+
+        txtFldExtra.textProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                    if (!newValue.matches("\\d*(\\" + separadorDecimal + "\\d*)?") && !newValue.isEmpty()) {
+                        txtFldExtra.setText(oldValue);
+                    } else {
+                        txtFldExtra.setText(newValue);
+                    }
+                }
+        );
+//        Mudar cor das linhas - não funcionando
+//        tbVwFuncs.setRowFactory(TableView
+//                -> {
+//            TableRow< Funcionario> row = new TableRow<>();
+//            row.itemProperty().addListener(
+//                    (observable, oldValue, newValue) -> {
+////                        if (newValue != null && (newValue.getClass().equals(Administrativo.class))) {
+////                            row.getStyleClass().add("administrativo");
+////                        } else {
+////                            row.getStyleClass().remove("administrativo");
+////                        }
+//                        if (newValue != null){
+//                            if(newValue.getClass().equals(Administrativo.class)){
+//                                 row.getStyleClass().add("administrativo");
+//                            }
+//                            else if(newValue.getClass().equals(Gerente.class)){
+//                                 row.getStyleClass().add("gerente");
+//                            }
+//                            else{
+//                                row.getStyleClass().add("vendedor");
+//                            }
+//                        }
+//                    });
+//            return row;
+//
+//        }
+//        );
+    }
+
+    private void limparCampos() {
+        txtFldNomeFunc.requestFocus();
+        rdBtnAdmSelect();
+        btnCadastrarFunc.disableProperty().bind(txtFldNomeFunc.textProperty().isEmpty().or(txtFldSalFunc.textProperty().isEmpty()));
+        txtFldNomeFunc.requestFocus();
+        txtFldNomeFunc.clear();
+        txtFldExtra.clear();
+        txtFldSalFunc.clear();
+        rdBtnAdm.setSelected(true);
+        rdBtnMasc.setSelected(true);
     }
 
     @FXML
@@ -121,42 +148,47 @@ public class PrincipalController implements Initializable {
         }
 
         if (rdBtnAdm.isSelected()) {
-            lstFuncionarios.add(new Administrativo(txtFldNomeFunc.getText(), Float.valueOf(txtFldSalFunc.getText()), sexo));
+            lstFuncionarios.add(0, new Administrativo(txtFldNomeFunc.getText(), Float.valueOf(txtFldSalFunc.getText()), sexo));
 
         } else if (rdBtnGerente.isSelected()) {
-            lstFuncionarios.add(new Gerente(txtFldNomeFunc.getText(), Float.valueOf(txtFldSalFunc.getText()), sexo, Float.valueOf(txtFldExtra.getText())));
+            lstFuncionarios.add(0, new Gerente(txtFldNomeFunc.getText(), Float.valueOf(txtFldSalFunc.getText()), sexo, Float.valueOf(txtFldExtra.getText())));
 
         } else {
-            lstFuncionarios.add(new Vendedor(txtFldNomeFunc.getText(), Float.valueOf(txtFldSalFunc.getText()), sexo, Float.valueOf(txtFldExtra.getText())));
+            lstFuncionarios.add(0, new Vendedor(txtFldNomeFunc.getText(), Float.valueOf(txtFldSalFunc.getText()), sexo, Float.valueOf(txtFldExtra.getText())));
 
         }
         tbVwFuncs.setItems(FXCollections.observableArrayList(lstFuncionarios));
-        
-        
-        txtFldNomeFunc.requestFocus();
-        txtFldNomeFunc.clear();
-        txtFldExtra.clear();
-        txtFldSalFunc.clear();
-        rdBtnGerente.setSelected(true);
-        rdBtnMasc.setSelected(true);
+
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Folha de pagamento");
+        alert.setHeaderText(null);
+        alert.setContentText(String.format("%s cadastrado com sucesso!", lstFuncionarios.get(0).getTipo().toUpperCase()));
+        alert.showAndWait();
+
+        limparCampos();
     }
 
     @FXML
     private void rdBtnGerenteSelect() {
         vBoxExtra.setVisible(true);
         lblExtra.setText("Total Bonificações");
+        btnCadastrarFunc.disableProperty().bind(txtFldNomeFunc.textProperty().isEmpty().or(txtFldSalFunc.textProperty().isEmpty()).or(txtFldExtra.textProperty().isEmpty()));
+
     }
 
     @FXML
-    private void rdBtnAdmSelect(ActionEvent event) {
+    private void rdBtnAdmSelect() {
+        txtFldExtra.clear();
         vBoxExtra.setVisible(false);
+        btnCadastrarFunc.disableProperty().bind(txtFldNomeFunc.textProperty().isEmpty().or(txtFldSalFunc.textProperty().isEmpty()));
+
     }
 
     @FXML
-    private void rdBtnVendSelect(ActionEvent event) {
+    private void rdBtnVendSelect() {
         vBoxExtra.setVisible(true);
         lblExtra.setText("Total Vendas");
-
+        btnCadastrarFunc.disableProperty().bind(txtFldNomeFunc.textProperty().isEmpty().or(txtFldSalFunc.textProperty().isEmpty()).or(txtFldExtra.textProperty().isEmpty()));
     }
 
 }
