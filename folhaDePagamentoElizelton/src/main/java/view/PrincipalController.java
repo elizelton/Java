541c93c0eb297352920884915d8a72b9f607326d
Elizelton Lisboa <elizeltonls@gmail.com>
2018-07-04 17:32:49 -0300

    Segunda entrega

--- folhaDePagamentoElizelton/src/main/java/view/PrincipalController.java
+++ folhaDePagamentoElizelton/src/main/java/view/PrincipalController.java
@@ -2,10 +2,14 @@ package view;
 
 import java.net.URL;
 import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -13,6 +17,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
@@ -62,6 +67,7 @@ public class PrincipalController implements Initializable {
     private List<Funcionario> lstFuncionarios = new ArrayList<Funcionario>();
 
     private final char separadorDecimal = new DecimalFormatSymbols(Locale.getDefault(Locale.Category.FORMAT)).getDecimalSeparator();
+    private final NumberFormat nf = NumberFormat.getInstance(Locale.getDefault());
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -70,7 +76,7 @@ public class PrincipalController implements Initializable {
         txtFldNomeFunc.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
-                if (!newValue.matches("\\D*") || newValue.matches("\\p{Punct}")) {
+                if (!newValue.matches("\\D*") || !newValue.matches("\\P{Punct}*") || newValue.length()>50) {
                     txtFldNomeFunc.setText(oldValue);
                 } else {
                     txtFldNomeFunc.setText(newValue.toUpperCase());
@@ -86,7 +92,6 @@ public class PrincipalController implements Initializable {
                     }
                 }
         );
-
         txtFldExtra.textProperty().addListener(
                 (observable, oldValue, newValue) -> {
                     if (!newValue.matches("\\d*(\\" + separadorDecimal + "\\d*)?") && !newValue.isEmpty()) {
@@ -148,18 +153,29 @@ public class PrincipalController implements Initializable {
         }
 
         if (rdBtnAdm.isSelected()) {
-            lstFuncionarios.add(0, new Administrativo(txtFldNomeFunc.getText(), Float.valueOf(txtFldSalFunc.getText()), sexo));
+            try {
+                lstFuncionarios.add(0, new Administrativo(txtFldNomeFunc.getText(), nf.parse(txtFldSalFunc.getText()).doubleValue(), sexo));
+            } catch (ParseException ex) {
+                System.out.println(ex);
+            }
 
         } else if (rdBtnGerente.isSelected()) {
-            lstFuncionarios.add(0, new Gerente(txtFldNomeFunc.getText(), Float.valueOf(txtFldSalFunc.getText()), sexo, Float.valueOf(txtFldExtra.getText())));
+            try {
+                lstFuncionarios.add(0, new Gerente(txtFldNomeFunc.getText(), nf.parse(txtFldSalFunc.getText()).doubleValue(), sexo, nf.parse(txtFldExtra.getText()).doubleValue()));
+            } catch (ParseException ex) {
+                System.out.println(ex);
+            }
 
         } else {
-            lstFuncionarios.add(0, new Vendedor(txtFldNomeFunc.getText(), Float.valueOf(txtFldSalFunc.getText()), sexo, Float.valueOf(txtFldExtra.getText())));
-
+            try {
+                lstFuncionarios.add(0, new Vendedor(txtFldNomeFunc.getText(), nf.parse(txtFldSalFunc.getText()).doubleValue(), sexo, nf.parse(txtFldExtra.getText()).doubleValue()));
+            } catch (ParseException ex) {
+                System.out.println(ex);
+            }
         }
         tbVwFuncs.setItems(FXCollections.observableArrayList(lstFuncionarios));
 
-        Alert alert = new Alert(AlertType.INFORMATION);
+        Alert alert = new Alert(AlertType.CONFIRMATION);
         alert.setTitle("Folha de pagamento");
         alert.setHeaderText(null);
         alert.setContentText(String.format("%s cadastrado com sucesso!", lstFuncionarios.get(0).getTipo().toUpperCase()));
@@ -191,4 +207,35 @@ public class PrincipalController implements Initializable {
         btnCadastrarFunc.disableProperty().bind(txtFldNomeFunc.textProperty().isEmpty().or(txtFldSalFunc.textProperty().isEmpty()).or(txtFldExtra.textProperty().isEmpty()));
     }
 
+//    public void cadFuncSucesso(){
+//        VBox root = new VBox();
+//        root.setPadding(new Insets(10));
+//        root.setSpacing(10);
+// 
+//        Button button3 = new Button("Information Alert without Header Text");
+// 
+//        button1.setOnAction(new EventHandler<ActionEvent>() {
+// 
+//            @Override
+//            public void handle(ActionEvent event) {
+//                showAlertWithHeaderText();
+//            }
+//        });
+// 
+//        button3.setOnAction(new EventHandler<ActionEvent>() {
+// 
+//            @Override
+//            public void handle(ActionEvent event) {
+//                showAlertWithoutHeaderText();
+//            }
+//        });
+// 
+//        root.getChildren().addAll(button1, button2, button3);
+// 
+//        Scene scene = new Scene(root, 450, 250);
+//        stage.setTitle("JavaFX Information Alert (o7planning.org)");
+//        stage.setScene(scene);
+// 
+//        stage.show();
+//    }
 }
